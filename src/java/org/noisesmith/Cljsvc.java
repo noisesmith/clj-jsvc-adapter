b1fbba84305e37cc28e2ab86169f6a0c9a7379ac
Justin Smith <noisesmith@gmail.com>
2017-09-14 21:45:44 -0700

    generates runnable jar that invokes clojure

--- src/java/org/noisesmith/Cljsvc.java
+++ src/java/org/noisesmith/Cljsvc.java
@@ -8,23 +8,36 @@ import clojure.lang.IFn;
 import clojure.java.api.Clojure;
 
 public class Cljsvc implements Daemon {
-  String ns_name = Clojure.read(System.getenv("clojure.app.ns"));
-  IFn require = Clojure.var("clojure.core", "require");
-  IFn resolve = Clojure.var("clojure.core", "ns-resolve");
+  static Object ns_name;
+  static IFn require;
+  static IFn resolve;
+  static {
+    ns_name = Clojure.read(System.getenv("clojure.app.ns"));
+    require = Clojure.var("clojure.core", "require");
+    resolve = Clojure.var("clojure.core", "ns-resolve");
+  }
   public void init(DaemonContext context)
-      throws DaemonInitException, Exception {
-    String fn_name = Clojure.read("init");
-    IFn ns = require(ns_name);
-    IFn ini_fn = resolve(ns_name, fn_name);
+    throws DaemonInitException, Exception {
+    String fn_name = (String) Clojure.read("init");
+    Object ns = require.invoke(ns_name);
+    IFn ini_fn = (IFn) resolve.invoke(ns_name, fn_name);
     ini_fn.invoke(context);
   }
   public void start() throws Exception {
     IFn prn = Clojure.var("clojure.core", "prn");
     Object data = Clojure.read("{:a 0 :b 1}");
-    prn.invoke("hello fro clojure", data);
+    prn.invoke("hello from clojure", data);
   }
   public void stop() throws Exception {
   }
   public void destroy() {
+  }
+  public static void main(String[] args) {
+    Cljsvc service = new Cljsvc();
+    try {
+      service.start();
+    } catch (Exception e) {
+      System.exit(0);
+    }
   }
 }
